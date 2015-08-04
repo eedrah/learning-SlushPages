3b67c1e634f9591eafdbdeab1e85ad98580de7d2
Eedrah <eedrah@users.noreply.github.com>
2015-08-04 20:59:40 +1200

    really basic setup

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("welcome to learning-SlushPages")
+var React = require('react')
+var r = React.DOM
+
+var App = React.createClass({
+  render: function () {
+    return r.span({}, 'wooo!')
+  }
+})
+
+React.render(
+  React.createElement(App, {}),
+  document.querySelector('main')
+)
